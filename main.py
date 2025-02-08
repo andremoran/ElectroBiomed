3db72b8c0060fe9f58b1b55438293c87903be661
andremoran <63021778+andremoran@users.noreply.github.com>
2025-02-08 15:31:57 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = Flask(__name__,
             template_folder='templates'
             )
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default_secret_key')
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 # Flask-Login configuration
 login_manager = LoginManager()
